e953c11e60871ca2019273926e114487fa45070b
Rémi <roeland.remi@yahoo.fr>
2021-11-26 16:55:00 +0100

    Add several methods

--- src/data/dataManager.js
+++ src/data/dataManager.js
@@ -1,9 +1,37 @@
 import data from '../data/data'
 
+/**
+ * Get User Main Data
+ *
+ * @param   {number}  id  User ID
+ *
+ * @return  {Object}  User Main Data
+ */
 function getUserMainData(id) {
   return data.USER_MAIN_DATA.find(user => {
-    return user.id === parseInt(id);
+    return user.id === id;
   })
+}
+
+/**
+ * Get User Activity
+ *
+ * @param   {number}  id  User ID
+ *
+ * @return  {Object}  User Activity
+ */
+function getUserActivity(id) {
+  const userData = data.USER_ACTIVITY.find(user => {
+    return user.userId === id;
+  })
+  const activity = userData.sessions;
+  let i = 0;
+  activity.map(session => {
+    i++;
+    return session.index = i;
+  })
+
+  return activity;
 }
 
 /**
@@ -11,7 +39,7 @@ function getUserMainData(id) {
  * 
  * @param   {number}  id  User ID
  *
- * @return  {array}  User Performances
+ * @return  {Array.<Object>}  User Performances
  */
 function getUserPerformances(id) {
   const userData = data.USER_PERFORMANCE.find(user => {
@@ -30,16 +58,38 @@ function getUserPerformances(id) {
   return performances;
 }
 
-function getUserAverageDuration(id) {
-  const userData = data.USER_AVERAGE_SESSIONS.find(user => {
+/**
+ * Get User Average Sessions
+ * 
+ * @param   {number}  id  User ID
+ *
+ * @return  {Array.<Object>}  User Average Sessions
+ */
+function getUserAverageSessions(id) {
+  return data.USER_AVERAGE_SESSIONS.find(user => {
     return user.userId === id
-  })
-  const sessions = userData.sessions;
-  return sessions;
+  }).sessions
+}
+
+function getUserKeyData(id) {
+  const userData = getUserMainData(id);
+  return userData.keyData;
+}
+
+function getUserScore(id) {
+  let scoreData = [];
+  let score = {}
+  const userData = getUserMainData(id);
+  score.value = userData.score * 100;
+  scoreData.push(score)
+  return scoreData;
 }
 
 export {
   getUserMainData,
+  getUserActivity,
   getUserPerformances,
-  getUserAverageDuration,
+  getUserAverageSessions,
+  getUserKeyData,
+  getUserScore,
 };
