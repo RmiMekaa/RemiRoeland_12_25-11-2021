0ee766f55b9f2c54a004f8fdfc9a55fc27bb873b
Rémi <roeland.remi@yahoo.fr>
2021-11-29 16:33:25 +0100

    Update AverageSessions component

--- src/components/AverageSessions/AverageSessions.jsx
+++ src/components/AverageSessions/AverageSessions.jsx
@@ -1,23 +1,66 @@
 import React from 'react';
-import { LineChart, Line, Tooltip, Legend, XAxis, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, Tooltip, XAxis, ResponsiveContainer } from 'recharts';
 import PropTypes from 'prop-types';
 
 function AverageSessions(props) {
 
+  // Format days from numbers to initials
+  const days = ['L', 'M', 'M', 'J', 'V', 'S', 'D']
+  props.data.map((item, i) => (item.day = days[i]))
+
   return (
     <div className='averageSessions'>
+      <h2 className='averageSessions__heading'>Durée moyenne des sessions</h2>
       <ResponsiveContainer width="100%" height="100%">
-        <LineChart width={730} height={250} data={props.data}>
-          <Tooltip />
-          <Line dot={false} legendType="none" type="monotone" dataKey="sessionLength" stroke="blue" />
+        <LineChart data={props.data} margin={{ top: 70, bottom: 10, left: 20, right: 20 }}>
+          <Tooltip
+            content={<CustomToolTipContent />}
+            cursor={<CustomCursor />}
+            contentStyle={{ border: 'none', background: 'white', }}
+            itemStyle={{ color: 'blue' }}
+          />
+          <XAxis dataKey="day" axisLine={false} tickLine={false} stroke="white" tickMargin={10} tick={{ fontSize: 12, opacity: 0.7 }} />
+          <Line dot={false} legendType="none" type="monotone" dataKey="sessionLength" stroke="white" strokeWidth="1.5" />
         </LineChart>
       </ResponsiveContainer>
-    </div>
+    </div >
   );
 }
 
+/**
+ * Custom tooltip content
+ * @param {object}    payload   data
+ * @param {boolean}   active    true if tooltip is displayed
+ */
+const CustomToolTipContent = ({ payload, active }) => {
+  if (active) {
+    return (
+      <div className="tooltip">
+        <p className="tooltip__text">{payload[0].value} min</p>
+      </div>
+    )
+  }
+  return null
+}
+
+/**
+ * Custom cursor
+ * @param {array} points coordinates 
+ */
+const CustomCursor = ({ points }) => {
+  return (
+    <rect
+      x={points[0].x}
+      y="0"
+      height="100%"
+      width="100%"
+      fill="rgba(0, 0, 0, 0.1)"
+    ></rect>
+  )
+}
+
 AverageSessions.propTypes = {
-  id: PropTypes.object.isRequired,
+  data: PropTypes.array.isRequired,
 };
 
 export default AverageSessions;
