c030db697447f8089c097da76467424ef26ebb14
Rémi <roeland.remi@yahoo.fr>
2021-11-30 13:28:36 +0100

    Rework dailyActivity component, create custom tooltip and heading

--- src/components/DailyActivity/DailyActivity.jsx
+++ src/components/DailyActivity/DailyActivity.jsx
@@ -1,27 +1,68 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import PropTypes from 'prop-types';
-import { BarChart, CartesianGrid, XAxis, YAxis, Legend, Bar, Tooltip } from 'recharts';
+import { ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Bar, Tooltip } from 'recharts';
 
+/**
+ * React Component for daily Activity
+ * @param {Array.<Object>} props data
+ * @returns {ReactElement}
+ */
 function DailyActivity(props) {
-
+  console.log(props);
   return (
     <div className='dailyActivity'>
-      <BarChart barGap={7} width={730} height={250} data={props.data} >
-        <Legend className='dailyActivity__legend' align="right" verticalAlign="top" iconType="circle" iconSize={9} />
-        <CartesianGrid strokeDasharray="2 3" vertical={false} />
-        <Tooltip />
-        <XAxis tickSize={0} dataKey={"index"} tickMargin={16} />
-        <YAxis hide={true} tickSize={0} orientation="right" />
-        <Bar name="poids (kg)" barSize={7} radius={[10, 10, 0, 0]} dataKey="kilogram" fill="#282D30" />
-        <Bar name="Calories brûlées (kCal)" barSize={7} radius={[10, 10, 0, 0]} dataKey="calories" fill="#E60000" />
-      </BarChart>
+      <TopSection />
+      <ResponsiveContainer width='100%' height='70%'>
+        <BarChart barGap={7} margin={{ bottom: 20, top: 20, left: 20, right: 0 }} data={props.data} >
+          <CartesianGrid strokeDasharray="2 3" vertical={false} />
+          <Tooltip itemStyle={{ color: 'white' }} contentStyle={{ border: 'none', color: 'white', background: '#E60000' }} content={< CustomToolTipContent />} />
+          <XAxis tickSize={0} dataKey={"index"} tickMargin={16} />
+          <YAxis dataKey="kilogram" tickSize={0} orientation="right" domain={['dataMin - 1', 'dataMax']} interval={0} axisLine={false} allowDecimals={false} />
+          <Bar dataKey="kilogram" barSize={7} radius={[10, 10, 0, 0]} fill="#282D30" />
+          <Bar dataKey="calories" barSize={7} radius={[10, 10, 0, 0]} fill="#E60000" />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 }
 
-DailyActivity.propTypes = {
-  id: PropTypes.object.isRequired,
-};
+/**
+ * React Element including barchart's name and legend
+ * @returns {ReactElement}
+ */
+const TopSection = () => {
+  return (
+    <div className='topSection'>
+      <h2 className='topSection__heading'>Activité quotidienne</h2>
+      <div className='topSection__legend'>
+        <span className='topSection__legend__weight'>Poids (kg)</span>
+        <span className='topSection__legend__calories'>Calories brûlées (kCal)</span>
+      </div>
+    </div>
+  )
+}
 
+/**
+ * Custom tooltip content
+ * @param {object}    payload   data
+ * @param {boolean}   active    true if tooltip is displayed
+ * @returns {ReactElement|null}
+ */
+const CustomToolTipContent = ({ payload, active }) => {
+  console.log(payload);
+  if (active) {
+    return (
+      <div className="barChartTooltip">
+        <span className="barChartTooltip__weigth">{payload[0].value}kg</span>
+        <span className="barChartTooltip__calories">{payload[1].value}Kcal</span>
+      </div>
+    )
+  }
+  return null
+}
+
+DailyActivity.propTypes = {
+  data: PropTypes.array.isRequired,
+};
 
 export default DailyActivity;
