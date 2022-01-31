f8ecd2dc182032e6f12bfa58c527c6bdcba3d127
Rémi <roeland.remi@yahoo.fr>
2022-01-31 16:24:32 +0100

    Code optimization

--- src/data/GetDataFromAPI.js
+++ src/data/GetDataFromAPI.js
@@ -1,11 +1,15 @@
 import axios from 'axios'
 
 //Routes
-const baseURL = "http://localhost:3000/user/";
-const averageSessionsUrl = (id) => { return baseURL + id + '/average-sessions' }
-const performanceUrl = (id) => { return baseURL + id + '/performance' }
-const activityUrl = (id) => { return baseURL + id + '/activity' }
-const mainDataUrl = (id) => { return baseURL + id }
+const baseURL = "http://localhost:3000/usder/";
+const urls = {
+  averageSessions: (id) => { return baseURL + id + '/average-sessions' },
+  performance: (id) => { return baseURL + id + '/performance' },
+  activity: (id) => { return baseURL + id + '/activity' },
+  score: (id) => { return baseURL + id },
+  userInfos: (id) => { return baseURL + id },
+  keyData: (id) => { return baseURL + id },
+}
 
 /**
  * Fetch data from API
@@ -15,28 +19,12 @@ const mainDataUrl = (id) => { return baseURL + id }
  */
 export async function getDataFromAPI(id, type) {
   try {
-    const requestUrl = setUrl(id, type);
-    const res = await axios(requestUrl);
-    return returnFromResponse(res, type);
+    const res = await axios(urls[type](id));
+    return dataFromResponse(res, type);
   }
   catch (err) {
     console.error(err);
     throw err;
-  }
-}
-
-/**
- * Config request url
- * @param   {Number}  id    User id
- * @param   {String}  type  Type of requested data
- * @return  {String}  Request url
- */
-function setUrl(id, type) {
-  switch (type) {
-    case 'averageSessions': return averageSessionsUrl(id);
-    case 'performance': return performanceUrl(id);
-    case 'activity': return activityUrl(id);
-    default: return mainDataUrl(id);
   }
 }
 
@@ -46,18 +34,12 @@ function setUrl(id, type) {
  * @param   {String}  type   Type of Requested data
  * @return  {Object}  Requested data
  */
-function returnFromResponse(res, type) {
-  switch (type) {
-    case 'averageSessions': return res.data.data.sessions;
-    case 'performance': return res.data.data;
-    case 'activity': return res.data.data.sessions;
-    case 'keyData': return res.data.data.keyData;
-    case 'userInfos': return res.data.data.userInfos;
-    case 'score': {
-      //Handle several property names for the user score
-      if (!res.data.data.score) return res.data.data.todayScore;
-      return res.data.data.score;
-    }
-    default: return;
-  }
+function dataFromResponse(res, type) {
+  const responseData = res.data.data;
+  if (type === 'performance') return responseData;
+  if (type === 'averageSessions' || type === 'activity') return responseData.sessions;
+  if (type === 'keyData') return responseData.keyData;
+  if (type === 'userInfos') return responseData.userInfos;
+  //Handle several property names issue for the user score
+  if (type === 'score') return (responseData.score ? responseData.score : responseData.todayScore)
 }
